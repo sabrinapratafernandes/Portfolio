e7f436675917fb53e3476d08496e6e81d051a6d7
Sabrina <sabrinabuenoprata@gmail.com>
2024-10-07 16:26:15 -0300

    optimizing the jobs component and creating its data file

--- src/components/Jobs.js
+++ src/components/Jobs.js
@@ -1,31 +1,18 @@
+import jobsData from "../data/jobsData"
+
 const Jobs = () => {
 
     return (
         <div className="flex flex-col items-center justify-center md:p-0 p-10 bg-principal text-white">
             <div className="m-4 flex flex-col items-center justify-center">
             <h2 className=" text-3xl pb-2  text-white">O que posso fazer por você:</h2>
-            <div className="flex flex-wrap">
-                <div className="p-2 m-2 flex flex-col items-center justify-center bg-white text-black w-64 h-20 rounded">
-                    <p>Ajuste visual de sites</p>
-                </div>
-                <div className="p-2 m-2 flex flex-col items-center justify-center bg-white text-black w-64 h-20 rounded">
-                    <p>Integração com wordpress para edição autonoma de conteúdos e blogs</p>
-                </div>
-                <div className="p-2 m-2 flex flex-col items-center justify-center bg-white text-black w-64 h-20 rounded">
-                    <p>Criação de novas páginas</p>
-                </div>
-                <div className="p-2 m-2 flex flex-col items-center justify-center bg-white text-black w-64 h-20 rounded">
-                    <p>Otimização da sua interface</p>
-                </div>
-                <div className="p-2 m-2 flex flex-col items-center justify-center bg-white text-black w-64 h-20 rounded">
-                    <p>Correção de bugs - Frontend</p>
-                </div>
-                <div className="p-2 m-2 flex flex-col items-center justify-center bg-white text-black w-64 h-20 rounded">
-                    <p>Consultoria para entender suas necessidades e ajudá-lo em priorizações</p>
-                </div>
-                <div className="p-2 m-2 flex flex-col items-center justify-center bg-white text-black w-64 h-20 rounded">
-                    <p>Criação de landing-pages (O cartão de visitas da web)</p>
-                </div>
+            <div className="flex flex-wrap justify-center align-center w-full">
+
+                {jobsData.map((job, index) => (
+                    <div className="p-4 m-2 flex flex-col items-center justify-center bg-white text-black  h-20 rounded hover:bg-yellow w-64 md:w-auto" key={index}>
+                    <p>{job.text}</p>
+                    </div>
+                ))}
                 
             </div>
             </div>
---
+++ src/data/jobsData.js
@@ -0,0 +1,25 @@
+const jobsData = [
+    {
+        text: 'Ajuste visual de sites'
+    },
+    {
+        text: 'Integração com wordpress para edição autonoma de conteúdos e blogs'
+    },
+    {
+        text: 'Criação de novas páginas'
+    },
+    {
+        text: 'Otimização da sua interface'
+    },
+    {
+        text: 'Correção de bugs - Frontend'
+    },
+    {
+        text: 'Consultoria para entender suas necessidades e ajudá-lo em priorizações'
+    },
+    {
+        text: 'Criação de landing-pages (O cartão de visitas da web)'
+    },
+];
+
+export default jobsData
